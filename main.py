917d646c6e4cfd1c727862563a2eb27f7ecd5a73
Mauro Ezequiel Bravo <mauroezequielbravo@hotmail.com>
2024-10-14 18:22:32 -0300

    Se agrega el control de la hora en GMT-3.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import schedule
+import time
 from os import path, getenv
 from os.path import join
 from dotenv import dotenv_values
 from functions import get_dolar, agregar_precios_dolar
+from datetime import datetime
+import pytz
 
 
 dotenv_path = join('.env')
@@ -18,9 +21,14 @@ def job():
     oficial = get_dolar(vars['URL'], vars['XPATH_OFICIAL'])
     agregar_precios_dolar(mep, ccl, cripto, tarjeta, blue, oficial)
 
+def is_time_to_run(hour, minute, timezone):
+    tz = pytz.timezone(timezone)
+    now = datetime.now(tz)
+    return now.hour == hour and now.minute == minute
+
 
 # schedule.every(2).minutes.do(job)
-# Programar el trabajo para que se ejecute de lunes a viernes a las 19h
+# Programar el trabajo para que se ejecute de lunes a viernes a las 19h GMT-3
 schedule.every().monday.at("19:00").do(job)
 schedule.every().tuesday.at("19:00").do(job)
 schedule.every().wednesday.at("19:00").do(job)
@@ -30,6 +38,8 @@ schedule.every().friday.at("19:00").do(job)
 # schedule.every(2).minutes.do(job)
 
 while True:
-    schedule.run_pending()
+    if is_time_to_run(19, 0, 'America/Sao_Paulo'):  # GMT-3
+        schedule.run_pending()
+    time.sleep(60)  # Esperar un minuto antes de verificar nuevamente
 
 
